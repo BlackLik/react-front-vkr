f0d97f73690fdec28a2dd71beb696142e0d69b36
Alexsandr <alssval@gmail.com>
2023-06-06 06:47:40 +0300

    Add gender page and start create home page

--- src/components/RussianMap/index.jsx
+++ src/components/RussianMap/index.jsx
@@ -1,16 +1,3 @@
-// import { Suspense, lazy } from 'react';
-// import { ProgressBar } from 'react-bootstrap';
 import RussianMap from './RussianMap';
-
-// function RussianMap() {
-//   const RussianMapLazy = lazy(() => import('./RussianMap'));
-//   return (
-//     <>
-//       <Suspense fallback={<ProgressBar now={100} animated />}>
-//         <RussianMapLazy />
-//       </Suspense>
-//     </>
-//   );
-// }
 
 export { RussianMap };
